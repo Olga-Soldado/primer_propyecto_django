681ffd274243501e64a58a1ce76b3131a2ed33c3
olga <valentina.soldado.1994@gmail.com>
2021-07-09 19:44:04 -0400

    revisando

--- first_app/urls.py
+++ first_app/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.root),
-    path('first_app/blogs', views.index),
-    path('first_app/blogs/new', views.new),
-    path('first_app/blogs/create', views.create),
-    path('first_app/blogs/<int:num>', views.show),
-    path('first_app/blogs/<int:num>/edit', views.edit),
-    path('first_app/blogs/<int:num>/delete', views.destroy),
-    path('first_app/blogs/bonus', views.bonus),
+    path('blogs', views.index),
+    path('blogs/new', views.new),
+    path('blogs/create', views.create),
+    path('blogs/<int:num>', views.show),
+    path('blogs/<int:num>/edit', views.edit),
+    path('blogs/<int:num>/delete', views.destroy),
+    path('blogs/bonus', views.bonus),
    
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
 # Create your views here.
 def root(request):
-    return redirect("first_app/blogs")
+    return redirect("/blogs")
 def index(request):
     return HttpResponse("placeholder para luego mostrar una lista de todos los blogs")
 def new(request):
